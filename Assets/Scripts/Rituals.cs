19a95e688ca04bff910a727984a1a7f9ba8a2709
Domas <navikasdomas@gmail.com>
2016-01-30 01:13:28 +0200

    Buttons Script Added

---
+++ Assets/Scripts/Rituals.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rituals : MonoBehaviour {
+
+
+    public int[] RuneSequenceA;
+    public int[] RuneSequenceB;
+    public int[] RuneSequenceC;
+    public int[] RuneSequenceD;
+    public int[] RuneSequenceActive;
+
+    private string disaster;
+    private int i = 0;
+    // Use this for initialization
+    void Start() {
+        disaster = "Storm";
+        for (int j = 0; j < 2; j++)
+        {
+            RuneSequenceA[j] = Random.Range(1, 5);
+            RuneSequenceB[j] = Random.Range(1, 5);
+            RuneSequenceC[j] = Random.Range(1, 5);
+            RuneSequenceD[j] = Random.Range(1, 5);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StormIsComing()
+    {
+        disaster = "Storm";
+    }
+
+    public void MosquitosAreComing()
+    {
+        disaster = "Mosquitos";
+    }
+
+    public void FloodIsComing()
+    {
+        disaster = "Flood";
+    }
+
+    public void HeatIsComing()
+    {
+        disaster = "Heat";
+    }
+
+
+
+
+
+    public void RunePressed1()
+    {
+        if(RuneSequenceActive.Length > i && RuneSequenceActive[i] == 1)
+        {
+            i++;
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("The rune is a lie");
+        }
+    }
+
+    public void RunePressed2()
+    {
+        if (RuneSequenceActive.Length > i && RuneSequenceActive[i] == 2)
+        {
+            i++;
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("The rune is a lie");
+        }
+    }
+
+    public void RunePressed3()
+    {
+        if (RuneSequenceActive.Length > i && RuneSequenceActive[i] == 3)
+        {
+            i++;
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("The rune is a lie");
+        }
+    }
+
+    public void RunePressed4()
+    {
+        if (RuneSequenceActive.Length > i && RuneSequenceActive[i] == 4)
+        {
+            i++;
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("The rune is a lie");
+        }
+    }
+
+    public void RunePressedA()
+    {
+        if (disaster == "Storm")
+        {
+            RuneSequenceActive = RuneSequenceA;
+        }
+        else
+        {
+            Debug.Log("Wrong Button Fag");
+        }
+        
+    }
+
+    public void RunePressedB()
+    {
+        if (disaster == "Mosquitos")
+        {
+            RuneSequenceActive = RuneSequenceB;
+        }
+        else
+        {
+            Debug.Log("Wrong Button Fag");
+        }
+    }
+
+    public void RunePressedC()
+    {
+        if (disaster == "Flood")
+        {
+            RuneSequenceActive = RuneSequenceC;
+        }
+        else
+        {
+            Debug.Log("Wrong Button Fag");
+        }
+    }
+
+    public void RunePressedD()
+    {
+        if (disaster == "Heat")
+        {
+            RuneSequenceActive = RuneSequenceD;
+        }
+        else
+        {
+            Debug.Log("Wrong Button Fag");
+        }
+    }
+}
