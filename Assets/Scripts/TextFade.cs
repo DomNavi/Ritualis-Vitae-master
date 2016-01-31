01de1a02bf8eefa5ca3a5558a57702081763a063
Domas <navikasdomas@gmail.com>
2016-01-31 09:07:56 +0200

    pize

--- Assets/Scripts/TextFade.cs
+++ Assets/Scripts/TextFade.cs
@@ -11,7 +11,6 @@ public class TextFade : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-      //  Temp = SequenceInfo.GetComponent<Text>().color;
         Temp = new Color (252f / 255f, 160f / 255f, 6f / 255f);
     }
 
@@ -20,13 +19,11 @@ public class TextFade : MonoBehaviour {
         if (fade)
         {
             Temp.a = Mathf.Lerp(Temp.a, 0.0f, 8.5f * Time.deltaTime);
-            //SequenceInfo.GetComponent<Text>().color = Temp;
             DisInfo.GetComponent<Text>().color = Temp;
         }
         else
         {
             Temp.a = Mathf.Lerp(Temp.a, 1.0f, 8.5f * Time.deltaTime);
-            //SequenceInfo.GetComponent<Text>().color = Temp;
             DisInfo.GetComponent<Text>().color = Temp;            
         }
 
